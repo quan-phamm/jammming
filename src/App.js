e6aa4ca234ffde98078fdd11efc4b295108c2aef
quan-phamm <minhquan270503@gmail.com>
2024-10-15 01:16:42 +0700

    Merge SearchBar.js to App.js Remove SearchBar.js to move the logic and state management into App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import SearchBarContainer from './components/SearchBarContainer';
+import React, { useState, useEffect } from 'react';
+import SearchBar from './components/SearchBar';
+import SearchResult from './components/SearchResult';
+import { getSearchResult } from './mock_data/fetch';
 
 function App() {
-  return (
-    <div className="App">
-      <SearchBarContainer />
-    </div>
-  );
-}
+  const [searchInput, setSearchInput] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [searchToggle, setSearchToggle] = useState(false);
+  const handleSearchInput = ({target}) => setSearchInput(target.value);
+
+  // Call HTTP requests upon submitting the search input
+  const handleSearchSubmit = e => {
+    e.preventDefault();
+    setSearchToggle(!searchToggle);
+  };
+
+  useEffect(() => {
+    //fetch data from spotify based on search result, called everytime user click Search or submit a search input
+    // jsonResponse = await response.json()
+    getSearchResult()
+      .then((jsonResponse) => {
+        const items = jsonResponse.tracks.items;
+        setSearchData(items.map((item) => {
+          const trackObject = {
+            id: item.id,
+            track: item.name,
+            artist: item.album.artists.map((artistObject) => artistObject.name)
+          };
+          return trackObject;  
+        }));
+        setSearchInput("");
+      })
+  }, [searchToggle]);
+
+
+    return (
+        <>
+          <SearchBar searchInput={searchInput} handleSearchInput={handleSearchInput} handleSearchSubmit={handleSearchSubmit}/>
+          <SearchResult searchData={searchData}/>
+        </>
+    );
+};
+
 
 export default App;
