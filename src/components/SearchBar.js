6319925420ed42ac25ca642a99b0ae6699410c2b
quan-phamm <minhquan270503@gmail.com>
2024-10-16 14:40:03 +0700

    Add aria-label and placeholder for Search input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ const SearchBar = ({ searchInput, handleSearchInput, handleSearchSubmit }) => {
     return (
         <div className="search-bar">
             <form onSubmit={handleSearchSubmit}>
-                <input id="search" type="text" value={searchInput} onChange={handleSearchInput}/>
+                <input id="search" type="text" name="search" aria-label='Search' placeholder='What do you want to search?' value={searchInput} onChange={handleSearchInput}/>
                 <button type="submit" className='search-btn'>Search</button>
             </form>
         </div>
