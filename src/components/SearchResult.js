8f98d6da23a29d4f888ff5719ca9aa8eff955eb5
quan-phamm <minhquan270503@gmail.com>
2024-10-16 12:08:04 +0700

    Shorten the code for rendering search result

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
 const SearchResult = ({ searchData }) => {
-    const trackLists = searchData.map(trackObj => {
-        return (
-            <div className='track' key={trackObj.id}>
-                <p className='track-name'>{trackObj.track}</p>
-                <p className='artists'>{trackObj.artist.join(", ")}</p>
-            </div>
-        )
-    });
-
     return (
         <div className='search-result'>
-            {trackLists}
+            {searchData.map(trackObj => (
+                <div className='track' key={trackObj.id}>
+                    <p className='track-name'>{trackObj.track}</p>
+                    <p className='artists'>{trackObj.artist.join(", ")}</p>
+                </div>
+            ))}
         </div>
     );
 };
