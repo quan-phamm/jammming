5d1e88fceaa20481dff4403b236c45fad70e51c4
quan-phamm <minhquan270503@gmail.com>
2024-10-16 12:37:24 +0700

    Add title for Search Result

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SearchResult = ({ searchData }) => {
     return (
         <div className='search-result'>
+            <h2>Search Result</h2>
             {searchData.map(trackObj => (
                 <div className='track' key={trackObj.id}>
                     <p className='track-name'>{trackObj.track}</p>
