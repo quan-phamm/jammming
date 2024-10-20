e59e6ef24d988196da160211fc1da1efd98bd561
quan-phamm <minhquan270503@gmail.com>
2024-10-20 19:19:44 +0700

    Focus on search bar as user click in the container

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './components_css/SearchBar.module.css';
 
 const SearchBar = ({ searchInput, handleSearchInput, handleSearchSubmit, resetSearchInput, refreshSearch }) => {
+    const searchInputRef = useRef();
+    const searchBarRef = useRef();
+    useEffect(() => {
+        const handleClick = () => {
+            searchInputRef.current.focus();
+        }
+        searchBarRef.current.addEventListener('click', handleClick);
+
+        return () => {searchBarRef.current.removeEventListener('click', handleClick);}
+    }, []);
+
     return (
         <div className="search-block">
             <form onSubmit={handleSearchSubmit}>
-                <div className={styles.searchBar}>
+                <div className={styles.searchBar} ref={searchBarRef}>
                     <div className={`${styles.icon} ${styles.searchIcon}`}>
                         <button type='submit' title='Search' onClick={handleSearchSubmit}>
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                         </button>
                     </div>
-                    <input id="search" type="text" name="search" aria-label='Search' placeholder='Search' value={searchInput} onChange={handleSearchInput}/>
-                    <div className={`${styles.icon} ${styles.clearSearchIcon}`}>
+                    <input id="search" type="text" name="search" aria-label='Search' placeholder='Search' value={searchInput} onChange={handleSearchInput} ref={searchInputRef}/>
+                    {searchInput && <div className={`${styles.icon} ${styles.clearSearchIcon}`}>
                         <button type='button' title='Clear' onClick={resetSearchInput}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" height="800px" width="800px" version="1.1" id="Capa_1" viewBox="0 0 460.775 460.775">
                                 <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55  c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55  c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505  c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55  l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719  c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
                             </svg>
                         </button>
-                    </div>
+                    </div>}
                 </div>            
                 <div className={`${styles.icon} ${styles.resetResultIcon}`}>
                     <button type='button' title='Refresh' onClick={refreshSearch}>
